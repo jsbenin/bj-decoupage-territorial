af13bd89eb5e68278a046ff91761606ba76f05d7
Alade Yessoufou <aladecharaf23@gmail.com>
2024-04-28 00:46:19 +0100

    fix the sql file data importation

--- seed.sh
+++ seed.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-DB_FILE=tmp/db.sqlite3
+# Replace with your database file path
+DB_FILE="tmp/db.sqlite3"
 
-SQL_FILE=data.sql
+# Replace with your SQL file path
+SQL_FILE="data.sql"
 
+# Check if the SQL file exists
 if [ ! -f "$SQL_FILE" ]; then
   echo "Error: SQL file '$SQL_FILE' does not exist!"
   exit 1
 fi
 
-sqlite3 $DB_FILE
-
-.read $SQL_FILE
-
-.quit
+# Open the SQLite command line tool
+sqlite3 "$DB_FILE" <"$SQL_FILE"
