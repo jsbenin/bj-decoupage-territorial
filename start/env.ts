11030f926d7325cb651304688d8a84660a8e5525
Alade Yessoufou <aladecharaf23@gmail.com>
2024-04-27 23:17:02 +0100

    fix sqlite driver

--- start/env.ts
+++ start/env.ts
@@ -23,9 +23,5 @@ export default await Env.create(new URL('../', import.meta.url), {
   | Variables for configuring database connection
   |----------------------------------------------------------
   */
-  DB_HOST: Env.schema.string({ format: 'host' }),
-  DB_PORT: Env.schema.number(),
-  DB_USER: Env.schema.string(),
-  DB_PASSWORD: Env.schema.string.optional(),
   DB_DATABASE: Env.schema.string()
 })
